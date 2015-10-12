2ca8534e46e8bf3b8bc2b9ce22b619e3d423a99b
alex <alex>
2015-10-12 17:56:34 +0400

    add eq

--- src/perm/SetOfPermutations.java
+++ src/perm/SetOfPermutations.java
@@ -2,6 +2,7 @@ package perm;
 
 import java.io.Serializable;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
@@ -60,15 +61,100 @@ public class SetOfPermutations implements Serializable, Iterable<Permutation> {
 		SetOfPermutations other = (SetOfPermutations) obj;
 		if (hashCode != other.hashCode)
 			return false;
-		if (!Arrays.equals(params, other.params))
-			return false;
+
+		return solve(other) != null;
+	}
+
+	public Permutation[] solve(SetOfPermutations other) {
 		if (permutationLength != other.permutationLength)
-			return false;
-		if (!Arrays.equals(permutations, other.permutations))
-			return false;
+			return null;
 		if (size != other.size)
-			return false;
-		return true;
+			return null;
+
+		Permutation[] a = this.permutations;
+		Permutation[] b = other.permutations;
+
+		int n = size, m = permutationLength;
+
+		if (n == 0 || m <= 1) {
+			return new Permutation[] { new Permutation(n), new Permutation(m) };
+		}
+
+		Permutation q = b[0];
+
+		if (n == 1) {
+			return new Permutation[] { new Permutation(n), a[0].invert().product(q) };
+		}
+
+		for (Permutation p : a) {
+			Permutation y = p.invert().product(q);
+
+			Permutation[] c = new Permutation[n];
+			Integer[] u = new Integer[n], v = new Integer[n];
+
+			for (int i = 0; i < n; i++) {
+				c[i] = a[i].product(y);
+				v[i] = u[i] = i;
+			}
+
+			if (ord(c, b, 0, 0, n, m, u, v)) {
+				Permutation x = new Permutation(v).invert().product(new Permutation(u));
+				return new Permutation[] { x, y };
+			}
+		}
+
+		return null;
+	}
+
+	private static class PermComp implements Comparator<Integer> {
+		int radix;
+		Permutation[] p;
+
+		public int compare(Integer i, Integer j) {
+			return Integer.compare(p[i].get(radix), p[j].get(radix));
+		}
+	}
+
+	private static final PermComp pc = new PermComp();
+
+	private static boolean ord(Permutation[] a, Permutation[] b, int radix, int from, int to, int m, Integer[] u, Integer[] v) {
+		if (to <= from) {
+			return true;
+		}
+
+		if (m <= radix) {
+			return true;
+		}
+
+		pc.radix = radix;
+
+		pc.p = a;
+		Arrays.sort(u, from, to, pc);
+
+		pc.p = b;
+		Arrays.sort(v, from, to, pc);
+
+		for (int i = from; i < to; i++) {
+			if (a[u[i]].get(radix) != b[v[i]].get(radix)) {
+				return false;
+			}
+		}
+
+		boolean ok = true;
+
+		int l = from;
+
+		while (ok && l < to) {
+			int r = l;
+
+			while (r < to && a[u[l]].get(radix) == a[u[r]].get(radix)) {
+				++r;
+			}
+			ok &= ord(a, b, radix + 1, l, r, m, u, v);
+			l = r;
+		}
+
+		return ok;
 	}
 
 	@Override
